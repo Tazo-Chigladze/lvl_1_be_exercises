bee9f7556e585bb9a10ba1fd07f687db76f17a91
Tazo-Chigladze <tazo.chigladze99@gmail.com>
2021-03-29 20:22:14 +0400

    delete

--- script.js
+++ script.js
@@ -24,15 +24,6 @@ secondVar = thirdVar;
 
 console.log('First: ', firstVar);
 console.log('Second:', secondVar);
-<<<<<<< HEAD
-console.log('hi')
-
-console.log('tazo');
-=======
-
-
-console.log('New console log');
->>>>>>> 3afb6988cc2775b822ae6270e78c3366e3a91e53
 
 
 console.log('tazoooo');
